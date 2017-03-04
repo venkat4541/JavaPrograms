7fc4a44e5e64334223aeb1a08346c49927304b9e
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-04 17:06:16 -0500

    More on methods

--- Methods/src/com/venkat/Main.java
+++ Methods/src/com/venkat/Main.java
@@ -4,11 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int finalScore = calculateScore(true, 800, 5, 100);
-        System.out.println(finalScore);
-        finalScore = calculateScore(false, 2000, 8, 300);
-        System.out.println(finalScore);
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Venkat", position);
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Polumahanti", position);
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Sana", position);
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("Charan", position);
+
+//        int finalScore = calculateScore(true, 800, 5, 100);
+//        System.out.println(finalScore);
+//        finalScore = calculateScore(false, 2000, 8, 300);
+//        System.out.println(finalScore);
     }
+
+
+    public static void displayHighScorePosition(String player, int pos) {
+        System.out.println(player + " managed to get into position " + pos + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if(score > 1000) {
+            return 1;
+        } else if(score > 500 && score <= 1000) {
+            return 2;
+        } else if(score > 100 && score <= 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
