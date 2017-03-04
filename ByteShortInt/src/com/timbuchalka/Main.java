9c31d5b2644d03d80e9711ff05045969a6ef80b1
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-04 14:16:21 -0500

    Byte, Int, Short, Long and type casting

---
+++ ByteShortInt/src/com/timbuchalka/Main.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int value = -2_147_483_648;
+	    System.out.println(value);
+	    byte val = -128;
+	    short vals = -32768;
+	    long valu = 1;
+    }
+}
