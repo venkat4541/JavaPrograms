642cb648dccc412914e55b67873133d789ad77ba
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-04 14:24:10 -0500

    Int and Booleans

---
+++ CharAndBoolean/src/com/timbuchalka/Main.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char sample = '\u00A9';
+	    System.out.println(sample);
+
+	    boolean bool = false;
+	    System.out.println(bool);
+    }
+}
